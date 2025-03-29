d676890fd741c95f29fdfe9da4c6696f7201a216
MrWhiteShapi <aigunovshapi@yandex.ru>
2025-03-29 10:20:30 +0300

    fix: Исправлен баг с LinkResultSetExtractor

--- scrapper/src/main/java/backend/academy/scrapper/repository/link/LinkResultSetExtractor.java
+++ scrapper/src/main/java/backend/academy/scrapper/repository/link/LinkResultSetExtractor.java
@@ -7,8 +7,11 @@ import backend.academy.scrapper.data.model.Tag;
 import jakarta.validation.constraints.NotNull;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
@@ -20,31 +23,52 @@ import org.springframework.stereotype.Component;
 public class LinkResultSetExtractor implements ResultSetExtractor<List<Link>> {
     @Override
     public @NotNull List<Link> extractData(ResultSet rs) throws SQLException, DataAccessException {
-        var links = new LinkedList<Link>();
+        Map<UUID, Link> linkMap = new HashMap<>();
         while (rs.next()) {
-            var link = Link.builder()
-                .id(UUID.fromString(rs.getString("id")))
-                .url(rs.getString("url"))
-                .lastUpdate(rs.getTimestamp("last_update").toLocalDateTime())
-                .build();
+            UUID linkId = UUID.fromString(rs.getString("id"));
+            Link link = linkMap.computeIfAbsent(linkId, id -> {
+                try {
+                    return Link.builder()
+                        .id(id)
+                        .url(rs.getString("url"))
+                        .lastUpdate(rs.getTimestamp("last_update").toLocalDateTime())
+                        .chats(new HashSet<>())
+                        .tags(new HashSet<>())
+                        .filters(new HashSet<>())
+                        .build();
+                } catch (SQLException e) {
+                    log.error("Error processing ResultSet row", e);
+                    throw new DataAccessException("Error processing ResultSet row", e) {};
+                }
+            });
 
-            link.chats().add(Chat.builder()
-                .id(UUID.fromString(rs.getString("chat_id")))
-                .tgId(rs.getLong("tg_id"))
-                .nickname(rs.getString("nickname"))
-                .build());
+            String chatIdStr = rs.getString("chat_id");
+            if (chatIdStr != null) {
+                link.chats().add(Chat.builder()
+                    .id(UUID.fromString(chatIdStr))
+                    .tgId(rs.getLong("tg_id"))
+                    .nickname(rs.getString("nickname"))
+                    .build());
+            }
 
-            link.tags().add(Tag.builder()
-                .id(UUID.fromString(rs.getString("tag_id")))
-                .tag(rs.getString("tag"))
-                .build());
+            String tagIdStr = rs.getString("tag_id");
+            if (tagIdStr != null) {
+                link.tags().add(Tag.builder()
+                    .id(UUID.fromString(tagIdStr))
+                    .tag(rs.getString("tag"))
+                    .build());
+            }
 
-            link.filters().add(Filter.builder()
-                .id(UUID.fromString(rs.getString("filter_id")))
-                .parameter(rs.getString("parameter"))
-                .value(rs.getString("value"))
-                .build());
+            String filterIdStr = rs.getString("filter_id");
+            if (filterIdStr != null) {
+                link.filters().add(Filter.builder()
+                    .id(UUID.fromString(filterIdStr))
+                    .parameter(rs.getString("parameter"))
+                    .value(rs.getString("value"))
+                    .build());
+            }
         }
+        var links = new ArrayList<>(linkMap.values());
         log.debug("Result set converted into List<Link>: {}", links);
         return links;
     }
