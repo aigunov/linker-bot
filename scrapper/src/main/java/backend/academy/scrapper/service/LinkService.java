d9e5e94bb744d0dcbe314544be00325ef70f3904
MrWhiteShapi <aigunovshapi@yandex.ru>
2025-04-05 11:57:24 +0300

    Исправлена ошибка в @ConditionalOnProperty

--- scrapper/src/main/java/backend/academy/scrapper/service/LinkService.java
+++ scrapper/src/main/java/backend/academy/scrapper/service/LinkService.java
@@ -16,6 +16,8 @@ import dto.ListLinkResponse;
 import dto.RemoveLinkRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.HashSet;
