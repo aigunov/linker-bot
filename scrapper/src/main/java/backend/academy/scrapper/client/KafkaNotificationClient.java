58be8e7e63775ed3c357d029ff3db1c156147666
MrWhiteShapi <aigunovshapi@yandex.ru>
2025-05-04 12:28:48 +0300

    feat: kafka клиент на scrapper

--- scrapper/src/main/java/backend/academy/scrapper/client/KafkaNotificationClient.java
+++ scrapper/src/main/java/backend/academy/scrapper/client/KafkaNotificationClient.java
@@ -1,13 +1,27 @@
 package backend.academy.scrapper.client;
 
 import dto.LinkUpdate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
-
+@Slf4j
+@Service
+@RequiredArgsConstructor
 @ConditionalOnProperty(value="app.message.transport", havingValue="Kafka")
 public class KafkaNotificationClient implements NotificationClient {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Value("${app.message.kafka.topic.notification}")
+    private String notificationTopic;
+
     @Override
     public void sendLinkUpdate(LinkUpdate linkUpdate) {
-
+        log.info("Sending link update to topic {}", notificationTopic);
+        kafkaTemplate.send(notificationTopic, linkUpdate);
     }
 }
